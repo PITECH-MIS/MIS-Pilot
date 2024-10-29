c2a1b9b9c06bdab8af00bfeea9b92d7d373705b7
Matr1x. <vip99013zhq@outlook.com>
2024-10-29 14:28:26 +0800

    24/10/29: Updated filter eth names

--- utils.cpp
+++ utils.cpp
@@ -6,7 +6,7 @@
 #include <QWidget>
 #include <QRegularExpression>
 
-QStringList filterEthWords = {"Miniport", "Virtual", "PPPoE", "Wi-Fi", "WiFi", "Bluetooth", "Tunnel", "VPN", "TEST", "loop"};
+QStringList filterEthWords = {"Miniport", "Virtual", "PPPoE", "Wi-Fi", "WiFi", "Bluetooth", "Tunnel", "VPN", "TEST", "loop", "TAP"};
 
 bool isUsableEth(QString& origin)
 {
