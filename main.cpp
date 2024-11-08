f15bbcb8d26b38b015f55fa538a6ab9bf2a69627
Matr1x. <vip99013zhq@outlook.com>
2024-11-09 01:10:10 +0800

    24/11/9: SCHED_RR on Linux PREEMPT_RT!

--- main.cpp
+++ main.cpp
@@ -7,10 +7,23 @@
 #pragma comment(lib, "ws2_32.lib")
 #endif
 
+#ifdef ENV_LINUX
+#include <sched.h>
+#endif
+
 int main(int argc, char *argv[])
 {
     // qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
     QApplication a(argc, argv);
+    QThread::currentThread()->setPriority(QThread::HighestPriority);
+#ifdef ENV_LINUX
+    struct sched_param param;
+    param.sched_priority = 99;
+    if(sched_setscheduler(0, SCHED_RR, &param) != 0)
+    {
+        qDebugMessage("Error on set Round-Robin Scheduler");
+    }
+#endif
     // setvbuf(stdout, NULL, _IONBF, 0);
     MainWindow w;
     w.show();
