0cc8cc697d7be6ce59e6e341e6b13be25986a5d7
Matr1x. <vip99013zhq@outlook.com>
2024-11-11 16:00:42 +0800

    24/11/11: Added PDOEndpointAccess

--- iFOC/PDOEndpointAccess.cpp
+++ iFOC/PDOEndpointAccess.cpp
@@ -1,3 +1,28 @@
 #include "PDOEndpointAccess.h"
 
-PDOEndpointAccess::PDOEndpointAccess() {}
+uint16_t getTrimmedSN(uint32_t SN)
+{
+    return (uint16_t)(SN % 10000);
+}
+
+PDOEndpointAccess* PDOEndpointAccess::getInstance()
+{
+    static PDOEndpointAccess access;
+    return &access;
+}
+
+void PDOEndpointAccess::SendEndpoint(const uint32_t SN, const PROTOCOL_ENDPOINT ep, const float value)
+{
+    auto sn = getTrimmedSN(SN);
+    protocol->print(0, "slave %d %d %.3f", sn, (uint16_t)ep, value);
+}
+
+void PDOEndpointAccess::TriggerIndicator(const uint32_t SN)
+{
+    SendEndpoint(SN, INDICATOR_TOGGLE, 1.0f);
+}
+
+void PDOEndpointAccess::SetHome(const uint32_t SN)
+{
+    SendEndpoint(SN, SET_HOME, 1.0f);
+}
--- iFOC/PDOEndpointAccess.h
+++ iFOC/PDOEndpointAccess.h
@@ -1,10 +1,22 @@
 #ifndef PDOENDPOINTACCESS_H
 #define PDOENDPOINTACCESS_H
 
+#include "../EtherCAT/PDOMasterProtocol.h"
+#include "endpoints_enum.h"
+
 class PDOEndpointAccess
 {
 public:
-    PDOEndpointAccess();
+    PDOEndpointAccess() = default;
+    void SendEndpoint(const uint32_t SN, const PROTOCOL_ENDPOINT ep, const float value);
+    void TriggerIndicator(const uint32_t SN);
+    void SetHome(const uint32_t SN);
+    // Singleton
+    static PDOEndpointAccess *getInstance();
+    PDOEndpointAccess(const PDOEndpointAccess &) = delete;
+    PDOEndpointAccess &operator=(const PDOEndpointAccess &) = delete;
+private:
+    PDOMasterProtocol* protocol = PDOMasterProtocol::getInstance();
 };
 
 #endif // PDOENDPOINTACCESS_H
